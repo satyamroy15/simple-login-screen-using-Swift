27c5742acfcf1ba38af613a232f36ca97a0fdf01
Satyam Roy <53702168+satyamroy15@users.noreply.github.com>
2022-06-26 15:15:40 +0530

    added simple login UI

---
+++ LoginScreenUI/Shared/LoginScreenUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginScreenUIApp.swift
+//  Shared
+//
+//  Created by Renitto Jose on 26/06/22.
+//
+
+import SwiftUI
+
+@main
+struct LoginScreenUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
